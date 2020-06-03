8558cf4df3c7819808ecece6ce1ec4ad7c99a072
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-06-03 01:48:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,9 +4,9 @@ attr_accessor :name, :grade
 attr_reader :id 
 
 def initialize(name, grade, id=nil)
-@name = name 
-@grade = grade
-@id = id
+  @name = name 
+  @grade = grade
+  @id = id
 end
   
 end
